4be7a8396e4d90791d80f987292edf2ed99cbb79
MSI\User <seanlin1326@gmail.com>
2023-09-01 07:56:23 +0800

    feat:1cinemachine。 2 new input system

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,33 +6,23 @@ namespace KitchenProject
     public class Player : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 7f;
+        [SerializeField] private GameInput gameInput;
 
         private bool isWalking;
         private void Update()
         {
-            Vector2 inputVector = new Vector2(0, 0);
 
-            if (Input.GetKey(KeyCode.W))
-            {
-               
-                inputVector.y = +1;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                inputVector.y = -1;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                inputVector.x = -1;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                inputVector.x = +1;
-            }
-            inputVector = inputVector.normalized;
-
+            Vector2 inputVector = gameInput.GetMovementVectorNormalized();
             Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            float playerSize = 0.7f;
+            bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+
+            if (canMove)
+            {
+                transform.position += moveDir * moveSpeed * Time.deltaTime;
+            }
+
+           
 
             isWalking = moveDir != Vector3.zero;
 
